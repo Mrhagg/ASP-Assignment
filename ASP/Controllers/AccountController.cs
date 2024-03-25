d1a48f607ff6e4055d4f6f096fc3952c47d1c204
Willi <william.hagg19982@outlook.com>
2024-03-25 19:47:31 +0100

    account init

--- ASP/Controllers/AccountController.cs
+++ ASP/Controllers/AccountController.cs
@@ -4,23 +4,5 @@ namespace ASP.Controllers;
 
 public class AccountController : Controller
 {
-    public IActionResult Index()
-    {
-        ViewData["Title"] = "Profile";
-        return View();
-    }
-
-    public IActionResult SignIn()
-    {
-        ViewData["Title"] = "Sign In";
-        return View();
-    }
-
-    public IActionResult SignUp()
-    {
-        ViewData["Title"] = "Sign Up";
-        return View();
-    }
-
     
 }
