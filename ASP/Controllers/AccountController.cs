53af75e08392009f80345f8d273cc2d334b2bf77
Willi <william.hagg19982@outlook.com>
2024-03-25 19:50:07 +0100

    account init

--- ASP/Controllers/AccountController.cs
+++ ASP/Controllers/AccountController.cs
@@ -4,5 +4,10 @@ namespace ASP.Controllers;
 
 public class AccountController : Controller
 {
-    
+    //privat readonly AccountService _accountService;
+
+    //public AccountController(AccountService accountService)
+    //{
+    //  _accountService = accountService;
+    //}
 }
